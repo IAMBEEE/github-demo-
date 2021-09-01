9a35be19ebb495ccca9896bcd407f87a89f3e458
IAMBEE <nguyentanloi00000@gmail.com>
2021-09-01 22:28:41 +0700

    TUI MOI TAP LAm

---
+++ Excute.java
@@ -0,0 +1,7 @@
+public class Excute {
+    public static void main(String[] args) {
+        Main myname = new Main();
+        myname.setName("tan loi");
+        System.out.println(myname.getName());
+    }
+}
